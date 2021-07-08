b170b28bf502c26e9f97a9da5f9bcdbf15c2c73b
joao.alves <joao.alves>
2021-07-08 19:22:46 -0300

    Adiciona exemplos da aula.

--- src/test/java/br/com/qm/elevador/ElevadorTests.java
+++ src/test/java/br/com/qm/elevador/ElevadorTests.java
@@ -1,8 +1,10 @@
 package br.com.qm.elevador;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class ElevadorTests {
@@ -10,19 +12,44 @@ public class ElevadorTests {
 	   @Test
 	    public void deveSubirUmAndarComSucesso() {
 		   
+		   Elevador elevador = new Elevador(5, 5);
+		   
+		   elevador.sobe();
+		   
+		   Assert.assertEquals(1, elevador.getAndarAtual());
 	    }
 
 	    @Test
 	    public void naoDeveSubirQuandoEstiverNoUltimoAndar() {
 
+	    	Elevador elevador = new Elevador(2, 2);
+	    	
+	    	elevador.sobe();
+	    	elevador.sobe(); // FAZ COM QUE O ELEVADOR ESTEJA NO ÚLTIMO ANDAR
+	    	
+	    	elevador.sobe(); // Não deve conseguir subir uma vez que já está no último andar.
+	    	
+	    	Assert.assertEquals(2, elevador.getAndarAtual());
 	    }
 
 	    @Test
 	    public void deveDescerUmAndar() {
+	    	Elevador elevador = new Elevador(4, 5);
+	    	
+	    	elevador.sobe();
+	    	elevador.desce();
+
+	    	Assert.assertEquals(0, elevador.getAndarAtual());
 	    }
 
 	    @Test
 	    public void naoDeveDescerQuandoEstiverNoPrimeiroAndar() {
+	    
+	    	Elevador elevador = new Elevador(4, 4);
+	    	
+	    	elevador.desce();
+	    
+	    	Assert.assertEquals(0, elevador.getAndarAtual());
 	    }
 
 	    @Test
